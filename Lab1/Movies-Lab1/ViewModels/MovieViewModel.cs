b54bf1ace5c0e9ac9cc5d9ad82796c6aa1a01b55
baltamas <70895239+baltamas@users.noreply.github.com>
2021-05-12 14:17:25 +0300

    -added viewmodels for movie

---
+++ Lab1/Movies-Lab1/ViewModels/MovieViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies_Lab1.ViewModels
+{
+    public class MovieViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int ReleaseYear { get; set; }
+        public string Genre { get; set; }
+        public float Rating { get; set; }
+        public string StudioName { get; set; }
+    }
+}
