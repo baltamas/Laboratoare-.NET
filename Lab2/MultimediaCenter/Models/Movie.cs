78631446548402062823da9c4f79c8d33d4b6426
baltamas <70895239+baltamas@users.noreply.github.com>
2021-05-12 18:31:27 +0300

    -project created for lab2

---
+++ Lab2/MultimediaCenter/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultimediaCenter.Models
+{
+    public class Movie
+    {
+        
+
+    }
+}
