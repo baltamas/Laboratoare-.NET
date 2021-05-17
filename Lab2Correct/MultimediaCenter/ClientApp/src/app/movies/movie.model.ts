58cecd0a4a41dca969e11b2c41841cd5d5c9c114
baltamas <70895239+baltamas@users.noreply.github.com>
2021-05-17 13:39:09 +0300

    -added movies to HTML

---
+++ Lab2Correct/MultimediaCenter/ClientApp/src/app/movies/movie.model.ts
@@ -0,0 +1,13 @@
+export class Movie {
+  Id: number;
+  Title: string;
+  Description: string;
+  Genre: string;
+  Duration: number;
+  ReleaseYear:number;
+  Director: string;
+  DateAdded: Date;
+  Rating: number;
+  Watched: boolean;
+
+}
